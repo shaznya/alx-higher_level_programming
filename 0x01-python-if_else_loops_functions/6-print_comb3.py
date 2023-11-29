ee1d931521297cccb3ef3cf18103f1abb7b0b41e
shaznya <shaznyagol@gmail.com>
2023-11-29 11:45:44 -0800

    The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 for tens_digit in range(10):
-    for units_digit in range(tens_digit, 10):
-        if tens_digit != units_digit:
-            print("{:d}{:d}".format(tens_digit, units_digit), end=', ' if tens_digit != 9 or units_digit != 9 else '\n')
+    for units_digit in range(tens_digit + 1, 10):
+        print("{:d}{:d}".format(tens_digit, units_digit), end=', ' if tens_digit != 9 or units_digit != 9 else '\n')
