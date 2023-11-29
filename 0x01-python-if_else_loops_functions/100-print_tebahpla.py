987198e7a156bc60ea38c5422f542680013e05f1
shaznya <shaznyagol@gmail.com>
2023-11-29 02:43:18 -0800

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for char_code in range(ord('z'), ord('a') - 1, -1):
-    print("{}{}".format(chr(char_code), chr(char_code - 32) if char_code % 2 == 1 else ''), end='')
+    print("{}{}".format(chr(char_code), chr(char_code - 32) if char_code % 2 == 1 and char_code != ord('a') else ''), end='')
