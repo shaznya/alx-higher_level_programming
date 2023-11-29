c40a0d40369fb896809f388a637f1586322922a6
shaznya <shaznyagol@gmail.com>
2023-11-29 02:39:36 -0800

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=", " if (i, j) != (8, 9) else "\n")
+
+for tens_digit in range(10):
+    for units_digit in range(tens_digit + 1, 10):
+        print("{:d}{:d}".format(tens_digit, units_digit), end=', ' if tens_digit != 9 or units_digit != 8 else '\n')
---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+def islower(c);
+def uppercase(str);
+def print_last_digit(number);
+def add(a, b);
+def pow(a, b);
+def fizzbuzz();
+listint_t *insert_node(listint_t **head, int number);
+
+#endif
