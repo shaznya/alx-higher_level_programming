51cd1f28e5b4da802585143e36c75e25757fb15e
shaznya <shaznyagol@gmail.com>
2023-11-29 03:39:37 -0800

    computes a to the power of b and return the value

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def pow(a, b):
+    result = 1
+
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    for _ in range(b):
+        result *= a
+
+    return result
+
+base = 2
+exponent = 3
+result = pow(base, exponent)
+print(result)
