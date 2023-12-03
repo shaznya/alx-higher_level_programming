3c44531cc1b5d703fbddf0e25d1fb55773028a31
shaznya <shaznyagol@gmail.com>
2023-12-03 10:47:27 -0800

    I am code x

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-import dis
-import hidden_4
+import marshal
 
-def print_names_from_pyc(module):
-    # Extract code objects from the module
-    code_objects = [item for item in module.__dict__.values() if callable(item)]
+def extract_names_from_pyc(pyc_file):
+    with open(pyc_file, 'rb') as file:
+        magic = file.read(4)
+        if magic != b'\x03\xf3\r\n':
+            raise ValueError("Not a valid Python 3.8 bytecode file.")
 
-    names = []
-    for code_object in code_objects:
-        if callable(code_object) and not code_object.__name__.startswith("__"):
-            names.append(code_object.__name__)
-    for name in sorted(names):
+        file.read(8)
+
+        names = set()
+        try:
+            while True:
+                try:
+                    code = marshal.load(file)
+                except EOFError:
+                    break
+
+                if isinstance(code, type((lambda: 0).__code__)):
+                    names.update(code.co_names)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    filtered_names = sorted(name for name in names if not name.startswith("__"))
+    for name in filtered_names:
         print(name)
 
 if __name__ == "__main__":
-    print_names_from_pyc(hidden_4)
+    extract_names_from_pyc("hidden_4.pyc")
