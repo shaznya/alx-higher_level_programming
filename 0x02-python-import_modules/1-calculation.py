50dbd6b74e1966c3bbe2ac3674a8d9d453bc60c1
shaznya <shaznyagol@gmail.com>
2023-12-03 04:52:59 -0800

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+result_add = add(a, b)
+result_subtract = sub(a, b)
+result_multiply = mul(a, b)
+result_divide = div(a, b)
+
+print(f"{a} + {b} = {result_add}")
+print(f"{a} - {b} = {result_subtract}")
+print(f"{a} * {b} = {result_multiply}")
+print(f"{a} / {b} = {result_divide}")
+
+if __name__ == "__main__":
+    pass
