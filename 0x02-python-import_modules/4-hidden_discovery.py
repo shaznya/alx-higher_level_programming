5249f9bc852b9ed5f8136a4a257d21db9fb0d9c2
shaznya <shaznyagol@gmail.com>
2023-12-03 10:38:51 -0800

    I am code x

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,12 +2,16 @@
 import dis
 import hidden_4
 
-def print_function_names(module):
-
+def print_names_from_pyc(module):
+    # Extract code objects from the module
     code_objects = [item for item in module.__dict__.values() if callable(item)]
 
+    names = []
     for code_object in code_objects:
         if callable(code_object) and not code_object.__name__.startswith("__"):
-            print(code_object.__name__)
+            names.append(code_object.__name__)
+    for name in sorted(names):
+        print(name)
 
-print_function_names(hidden_4)
+if __name__ == "__main__":
+    print_names_from_pyc(hidden_4)
