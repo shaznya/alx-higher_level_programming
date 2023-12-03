fc23386a380071551f7ad5b72895f7378cae4ef1
shaznya <shaznyagol@gmail.com>
2023-12-03 07:44:23 -0800

    i am code x

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import dis
+import hidden_4
+
+def print_function_names(module):
+    # Extract code objects from the module
+    code_objects = [item for item in module.__dict__.values() if callable(item)]
+
+    for code_object in code_objects:
+        if callable(code_object) and not code_object.__name__.startswith("__"):
+            print(code_object.__name__)
+
+print_function_names(hidden_4)
