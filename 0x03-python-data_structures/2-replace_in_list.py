1037f473893375b591dec431edd89198b7b952af
shaznya <shaznyagol@gmail.com>
2023-12-04 12:58:14 -0800

    Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    for i, current_element in enumerate(my_list):
+        if i == idx:
+            my_list[i] = element
+            break
+
+    return my_list
