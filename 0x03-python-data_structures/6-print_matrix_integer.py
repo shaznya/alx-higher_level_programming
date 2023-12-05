962e76cf12e5f33ac154a5c75c098fdf1f2d1eed
shaznya <shaznyagol@gmail.com>
2023-12-05 01:18:38 -0800

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+        return
+
     for row in matrix:
         for elem in row:
             print("{:d}".format(elem), end=" ")
