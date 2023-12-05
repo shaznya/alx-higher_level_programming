603809d4aff3e8f1f1ca4060f391916125a15417
shaznya <shaznyagol@gmail.com>
2023-12-05 00:25:23 -0800

    Yes i can C you

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
-    return new_string
+    result = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            result += char
+    return result
