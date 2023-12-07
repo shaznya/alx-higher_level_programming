b3704e88a4f3d079e881468a9682f742aae0f7b7
shaznya <shaznyagol@gmail.com>
2023-12-06 18:50:30 -0800

    I am who i am

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 import importlib.util
 
-def print_module_names(file_path):
-    module_name = "module_name"  # Choose a name for the temporary module
-
+def get_names_from_pyc(file_path):
+    # Create a new module
+    module_name = 'dummy_module'
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
-
     names = [name for name in dir(module) if not name.startswith('__')]
 
-    for name in sorted(names):
-        print(name)
+    names.sort()
+
+    return names
 
 if __name__ == "__main__":
-    file_path = "hidden_4.pyc"
-    print_module_names(file_path)
+    pyc_file_path = 'hidden_4.pyc'
+    names = get_names_from_pyc(pyc_file_path)
+    for name in names:
+        print(name)
