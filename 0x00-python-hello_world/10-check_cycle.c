2cb6bc6a240680112c7120992489d98a497b81a8
shaznya <shaznyagol@gmail.com>
2024-01-15 12:22:28 -0800

    0x00-python-hello_world

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,40 +2,30 @@
 #include <stdlib.h>
 
 /* Definition for singly-linked list. */
-struct listint {
-    int value;
-    struct listint *next;
-};
-
-typedef struct listint listint_t;
+typedef struct listint_t {
+    int data;
+    struct listint_t *next;
+} listint_t;
 
 /* Function to check if a singly linked list has a cycle. */
-int check_cycle(listint_t *list);
-
-/* Example usage */
-int main()
+int check_cycle(listint_t *list)
 {
-	int result;
-    listint_t *head = malloc(sizeof(listint_t));
-    head->value = 1;
-    head->next = malloc(sizeof(listint_t));
-    head->next->value = 2;
-    head->next->next = malloc(sizeof(listint_t));
-    head->next->next->value = 3;
-    head->next->next->next = head;
-
-    result = check_cycle(head);
-
-    if (result) {
-        printf("The linked list has a cycle.\n");
-    } else {
-        printf("The linked list does not have a cycle.\n");
-    }
-
-    /* Remember to free allocated memory */
-    free(head->next->next);
-    free(head->next);
-    free(head);
-
-    return 0;
+	listint_t *tortoise, *hare;
+	if (list == NULL || list->next == NULL)
+	{
+		return 0;
+	}
+	tortoise = list;
+	hare = list->next;
+	while (hare != NULL && hare->next != NULL)
+	{
+		if (tortoise == hare)
+		{
+			return 1;
+		}
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+	return 0;
 }
+
