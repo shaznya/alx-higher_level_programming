b83e97d570f8d8d025699c0ca4c13f942c69f8c9
shaznya <shaznyagol@gmail.com>
2024-01-17 03:57:16 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-def pow(a, b, precision=10):
+def pow(a, b):
     result = 1
+    
+    # If b is negative, calculate reciprocal of a raised to abs(b)
     if b < 0:
         a = 1 / a
-        b = -
+        b = abs(b)
+    
+    # Calculate a^b using a loop
     for _ in range(b):
         result *= a
-
-    return round(result, precision
+    
+    return result
