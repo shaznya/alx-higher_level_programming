6a59b01f436301d011d939ee844984046dd8dbcc
shaznya <shaznyagol@gmail.com>
2024-01-18 11:40:58 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from  hidden_4 import *
-    for i in range(hidden_4):
-        print(f"{*[i]} {hidden_4[i]}")
+import marshal
+
+def print_module_names(file_path):
+    with open(file_path, 'rb') as f:
+        code = marshal.load(f)
+
+    module_names = [name for name in code.co_names if not name.startswith('__')]
+    module_names.sort()
+
+    for name in module_names:
+        print(name)
+        print_module_names('hidden_4.pyc')
