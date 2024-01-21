32bf7ff12b4414d75719c0974ce60487f7fb90c6
shaznya <shaznyagol@gmail.com>
2024-01-21 03:56:36 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result = ""
     for char in my_string:
-        if char.lower() != 'c':
+        if char.lower() != 'c' and char.lower() != 'C':
             result += char
             return result
