2b8fca458c2387f4d0de7c09316146d1fca68f84
shaznya <shaznyagol@gmail.com>
2024-01-22 23:15:49 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).add, __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).sub
-
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0).add, __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0).sub
+    
     if a < b:
         c = add(a, b)
+        
         for i in range(4, 6):
             c = add(c, i)
+        
         return c
     else:
         return sub(a, b)
