ebc2f0a0c463824d38729216a5f5a0b95608bcde
shaznya <shaznyagol@gmail.com>
2024-01-24 11:20:33 -0800

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    result_matrix = [[0] * len(row) for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result_matrix[i][j] = matrix[i][j] ** 2
+
+    return result_matrix
