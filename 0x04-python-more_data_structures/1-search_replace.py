ded13cd82ad8f2097e61e9084f9763e8f877eb5f
shaznya <shaznyagol@gmail.com>
2024-01-25 00:32:34 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    replace_func = lambda x: replace if x == search else x
-    new_list = list(map(replace_func, my_list))
+    new_list = list(map(lambda t: replace if t[1] == search else t[1], enumerate(my_list)))
     return new_list
