c9300cc70333d8097ba23f67940f66258d662999
shaznya <shaznyagol@gmail.com>
2024-01-25 00:36:45 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [replace if element == search else element for index, element in enumerate(my_list)]
+     new_list = [replace if item == search else item for item in my_list]
     return new_list
