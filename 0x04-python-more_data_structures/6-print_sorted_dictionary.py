95b0b71d845117cb2b14e202e18f911b6f1e71ac
shaznya <shaznyagol@gmail.com>
2024-01-27 08:52:16 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        print("Error: Input dictionary is None")
+        return
+
     sorted_keys = sorted(a_dictionary.keys())
 
     for key in sorted_keys:
