183befac130c4c6f5df670e5088be73e7be7a045
shaznya <shaznyagol@gmail.com>
2024-01-27 00:30:57 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_3 = set()
-    for elem in set_1:
-        if elem in set_2:
-            set_3.add(elem)
-            return set_3
+    common_set = set_1.intersection(set_2)
+    return common_set
