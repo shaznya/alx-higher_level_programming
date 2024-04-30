f9de4693a7a90e35a46d0c836e10c1b5ced43416
shaznya <shaznyagol@gmail.com>
2024-04-30 02:52:52 -0700

    0x00-python-hello_world

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[70:]+str[0:6]+str[6:6+1]+str[6+1:6+11]+str[6+11:6+11+2]+str[6+11+2:6+11+2+17])
+print(str[43:72] + "\n")
 print(str)
