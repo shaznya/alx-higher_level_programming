a4d7e39d37dc2e8fee13c8d4dd7c9021c66ad42b
shaznya <shaznyagol@gmail.com>
2024-05-08 00:12:06 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
-    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
+#!/usr/bin/python3
+for c in range(ord('z'), ord('A') - 1, -1):
+    print("{:c}".format(c if c % 2 == 1 else c - 32), end="")
