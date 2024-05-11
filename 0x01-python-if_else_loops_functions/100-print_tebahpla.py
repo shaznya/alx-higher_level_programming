6d77b0cf9ed3f9e4a891775048ffb835b5cb74a4
shaznya <shaznyagol@gmail.com>
2024-05-11 03:02:43 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{:c}".format(i) if (122 - i) % 2 == 0 else "{:c}".format(i).upper(), end="")
+    char = "{:c}".format(i)
+    print(char if (122 - i) % 2 == 0 else char.upper(), end="")
