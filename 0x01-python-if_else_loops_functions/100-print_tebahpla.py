75913a4665ff06c378c1a5ce40f7548d226274a1
shaznya <shaznyagol@gmail.com>
2024-05-10 23:59:29 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 output = ''
 for i in range(ord('z'), ord('a') - 1, -1):
-    output += '{}'.format(chr(i % 2 * 32 + i))
+    output += chr(i) if (ord('z') - i) % 2 == 0 else chr(i).upper()
+
 print(output, end='')
