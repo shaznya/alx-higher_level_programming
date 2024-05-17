c36f4a7c2afd55d6eae3c905640d81237b629176
shaznya <shaznyagol@gmail.com>
2024-05-17 05:29:07 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     a = 10
     b = 5
+    
     add_result = add(a, b)
     sub_result = sub(a, b)
     mul_result = mul(a, b)
-    div_result = div (a, b)
+    div_result = div(a, b)
 
     print("{} + {} = {}".format(a, b, add_result))
     print("{} - {} = {}".format(a, b, sub_result))
