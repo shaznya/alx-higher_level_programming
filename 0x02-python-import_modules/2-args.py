3d1b6d77dd44b9ed8e11ce017e8d641f6f522e41
shaznya <shaznyagol@gmail.com>
2024-05-20 01:03:01 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__":
-     main()
 
 def main():
     argv = sys.argv[1:]
@@ -13,3 +11,6 @@ def main():
         print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
         for i, arg in enumerate(argv, start=1):
             print(f"{i}: {arg}")
+
+if __name__ == "__main__":
+    main()
