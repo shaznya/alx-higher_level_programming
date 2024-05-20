50ac73201db7d4fe6b1ec6e96a0f690305b7c9a6
shaznya <shaznyagol@gmail.com>
2024-05-20 00:48:39 -0700

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    argv = sys.argv[1:]
+    num_args = len(argv)
+
+    if num_args == 0:
+        print(f"{num_args} arguments.")
+    else:
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
+
+if __name__ == "__main__":
+    main()
