d2ea8e85da39045de712a74dea4d24cff3cdc2b1
shaznya <shaznyagol@gmail.com>
2024-06-10 00:11:32 -0700

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,3 +2,8 @@
 class Square:
     """Represents a square shape."""
     pass
+# Main code
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
