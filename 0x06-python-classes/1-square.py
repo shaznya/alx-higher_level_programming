58a752c0298ffbb5837f94b3b214bfac501b1e3e
shaznya <shaznyagol@gmail.com>
2024-06-11 11:51:02 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    """Square class with a private instance attribute size."""
+    
+    def __init__(self, size):
+        """Initialize the Square class with a given size."""
+        self.__size = size
