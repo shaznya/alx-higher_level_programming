bc21970f89602c01407a2d504d4bf4e242d75e90
shaznya <shaznyagol@gmail.com>
2024-06-12 00:57:05 -0700

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,13 +3,12 @@
 This module defines an empty class Square.
 """
 
+
 class Square:
     """
     This class defines a square.
     """
-    pass
+
 
 if __name__ == "__main__":
-    # This block is executed only if this script is run directly
-    # For now, it doesn't do anything
     pass
