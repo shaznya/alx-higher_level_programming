68b3deb65c20fa3616a5928c2c45b787293f37a0
shaznya <shaznyagol@gmail.com>
2024-07-20 23:58:22 -0700

    0x07-python-test_driven_development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Module to add two integers.
+"""
+
+def add_integer(a, b=98):
+    """
+    Add two integers.
+
+    Parameters:
+    a (int, float): The first number.
+    b (int, float, optional): The second number. Defaults to 98.
+
+    Returns:
+    int: The sum of a and b.
+
+    Raises:
+    TypeError: If a or b is not an integer or float.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,1 +1,15 @@
-0x07-python-test_driven_development
+# My Project
+
+This project includes a Python module and test cases for adding two integers.
+
+## Files
+
+- `0-add_integer.py`: Contains the `add_integer` function.
+- `0-main.py`: Script to test the `add_integer` function.
+- `tests/0-add_integer.txt`: Doctests for the `add_integer` function.
+
+## Usage
+
+To run the interactive test script:
+```sh
+./0-main.py
