2430019364f24e75a0d9e3a28ced8e5796304abf
shaznya <shaznyagol@gmail.com>
2024-07-31 10:42:37 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -10,3 +10,16 @@ def lookup(obj):
         A list of strings representing the names of attributes and methods.
     """
     return dir(obj)
+
+if __name__ == "__main__":
+    class MyClass1:
+        """A sample class for demonstration."""
+        pass
+
+    class MyClass2:
+        """Another sample class with an attribute and a method."""
+        my_attr1 = 3
+
+        def my_meth(self):
+            """A sample method."""
+            pass
