2aa95da4a0c7a8093a46e5393868774036481a02
shaznya <shaznyagol@gmail.com>
2024-08-06 00:39:43 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
+"""
+This module defines a class MyList that inherits from the built-in list class.
+"""
+
 
 class MyList(list):
     """
-    A class that inherits from the built-in list and includes
-    a method to print the list in ascending order.
+    MyList class that inherits from list.
     """
 
     def print_sorted(self):
         """
-        Prints the list in ascending sorted order.
-        Assumes that all elements in the list are integers.
+        Print the elements of the list in ascending order.
         """
-        # Create a copy of the list
-        sorted_list = self[:]  
-        
-        # Simple sorting algorithm (Bubble Sort)
-        for i in range(len(sorted_list)):
-            for j in range(i + 1, len(sorted_list)):
-                if sorted_list[i] > sorted_list[j]:
-                    sorted_list[i], sorted_list[j] = sorted_list[j], sorted_list[i]
-        
+        sorted_list = sorted(self)
         print(sorted_list)
