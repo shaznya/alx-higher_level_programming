b83e0279d794b89cb6ce863d0506c969317db27b
shaznya <shaznyagol@gmail.com>
2024-08-16 01:42:12 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,3 +4,8 @@ def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
+
+if __name__ == "__main__":
+
+    filename = "example.txt"
+    read_file(filename)
