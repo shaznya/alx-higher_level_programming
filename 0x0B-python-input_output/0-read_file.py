471ca098fe0e08ca27caf2f0efeab40d0edaaff8
shaznya <shaznyagol@gmail.com>
2024-08-16 02:21:50 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""Reads a UTF8 text file and prints its content to stdout."""
+
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
+    """Reads a text file (UTF8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end="")
+        content = file.read()
+        print(content, end='')
 
-if __name__ == "__main__":
 
-    filename = "example.txt"
-    read_file(filename)
+if __name__ == '__main__':
+    pass
