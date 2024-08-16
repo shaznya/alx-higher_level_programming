1f5be6f921d7ea6d4c4d8fe635be792e9aa67aa8
shaznya <shaznyagol@gmail.com>
2024-08-16 09:24:02 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Writes a string to a text file (UTF8) and returns the number of characters written."""
+"""Writes a string to a text file (UTF8)."""
+
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written.
+    """Writes a string to a text file (UTF8).
 
     Args:
-        filename (str): The name of the file to write to. Defaults to an empty string.
+        filename (str): The name of the file to write to.
         text (str): The text to write to the file. Defaults to an empty string.
 
     Returns:
