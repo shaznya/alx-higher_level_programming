ae1b30e23e13428e81715e19858f7cff188522d4
shaznya <shaznyagol@gmail.com>
2024-08-20 00:33:17 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,12 +2,28 @@
 
 
 class MyList(list):
-    """A class that inherits from list and provides a method to print the list in sorted order."""
+    """
+    A subclass of list that includes a method to print the list in ascending order.
+    """
 
     def print_sorted(self):
-        """Prints the list in ascending order."""
-        print(sorted(self))
+        """
+        Prints the list in ascending order.
+        """
+        sorted_list = self[:]
+        sorted_list.sort()
+        print(sorted_list)
+
 
 if __name__ == "__main__":
-    my_list = MyList([4, 2, 9, 1, 5, 6])
+    my_list = MyList([3, 1, 2])
+    my_list.print_sorted()
+
+    my_list = MyList([5, 7, 2, 4])
+    my_list.print_sorted()
+
+    my_list = MyList([10, 5])
+    my_list.print_sorted()
+
+    my_list = MyList([])
     my_list.print_sorted()
