b60e95ff26d00de233a810d7f7af3759ce94b34d
shaznya <shaznyagol@gmail.com>
2025-03-19 23:12:08 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(i) for i in range(97, 123)
-      if i not in (101, 113)), end="")
+print(
+    "{}".format(
+        "".join(chr(i) for i in range(97, 123) if i not in (101, 113))
+    ), end=""
+)
