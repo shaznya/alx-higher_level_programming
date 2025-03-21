ad655eaa92a756d1290eb436426bd85ceca8f8e3
shaznya <shaznyagol@gmail.com>
2025-03-20 23:36:46 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-# Print lowercase ASCII alphabet excluding 'q' and 'e' in one print statement
-print("".join(
-    "{:c}".format(letter) for letter in range(97, 123)
-    if letter not in (113, 101)
-), end="")
+print("".join("{:c}".format(letter) for letter in range(97, 123)
+              if letter not in (101, 113)), end="")
