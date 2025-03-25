37b623aac5b7c18721362643807c6d4fd37dcceb
shaznya <shaznyagol@gmail.com>
2025-03-24 23:26:01 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 
-print("".join("{}".format(chr(c - (32 * (c % 2)))) for c in range(122, 96, -1)), end="")
+print("".join(
+    "{}".format(chr(c - (32 * (c % 2)))) for c in range(122, 96, -1)
+), end="")
