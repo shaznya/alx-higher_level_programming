4f839015d835dbac4f1a5726f22654b1f3a87a0b
shaznya <shaznyagol@gmail.com>
2025-03-28 02:30:43 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-import importlib.util
+import hidden_4
+
 
 if __name__ == "__main__":
-    module_name = "hidden_4"
-    module_path = "./hidden_4.pyc"
-
-    spec = importlib.util.spec_from_file_location(module_name, module_path)
-    hidden_4 = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(hidden_4)
-
-    names = dir(hidden_4)
-
-    filtered_names = sorted(name for name in names if not name.startswith("__"))
-
-    for name in filtered_names:
-        print(name)
+    names = [name for name in dir(hidden_4) if not name.startswith('__')]
+    print(names)
